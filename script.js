bb9ecb2f9416445c0dc9a95844cd476b447a493b
Jonathan <jonathan16official@gmail.com>
2022-04-28 00:26:21 +0530

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const videoCardContainer = document.querySelector('.video-container');
+const videoCardContainer = document.querySelector('.video-cont');
 
 let api_key = "AIzaSyB--Hrjv_v4S_hBLhfhXtR1YlVyFkdIslw";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 30,
+    regionCode: 'IN'
+}))
+    .then(res => res.json())
+    .then(data => {
+        data.items.forEach(item => {
+            getChannelIcon(item);
+        })
+    })
+    .catch(err => console.log(err));
 
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+        .then(res => res.json())
+        .then(data => {
+            video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+            makeVideoCard(video_data);
+        })
+}
 const makeVideoCard = (data) => {
     videoCardContainer.innerHTML += `
     <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
